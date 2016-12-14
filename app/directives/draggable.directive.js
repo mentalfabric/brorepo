33ee53bf247eb8b1a773b32b024bef5daee3cd3d
Michael Wallert <michael.wallert@hotmail.com>
2016-12-13 20:30:35 -0800

    Almost finished validation for the container of the draggable image.

--- app/directives/draggable.directive.js
+++ app/directives/draggable.directive.js
@@ -3,7 +3,7 @@ angular.module('drag', [])
 
   function draggable($document){
     return function(scope, element, attr) {
-      var startX = 0, startY = 0, x = 0, y = 0, max-y = jQuery('.container--box').css('height'), max-x = jQuery('.container--box').css('width');
+      var startX = 0, startY = 0, x = 0, y = 0, maxY = jQuery('.container--box').height(), maxX = jQuery('.container--box').width();
       element.on('mousedown', function(event) {
         // Prevent default dragging of selected content
         event.preventDefault();
@@ -16,6 +16,34 @@ angular.module('drag', [])
       function mousemove(event) {
         y = event.screenY - startY;
         x = event.screenX - startX;
+
+        if(x >= maxX && y >= maxY){
+          x = maxX - 5;
+          y = maxY - 5;
+        } else if(x <= 0 && y <= 0){
+          x = 5;
+          y = 5;
+        } else if(x <= 0 && y >= maxY - 20){
+          x = 5;
+          y = maxY - 20;
+        } else if(y <= 0 && x >= maxX - 20){
+          y = 5;
+          x = maxX - 20;
+        } else if(x >= maxX - 60 && y >= maxY - 20){
+          y = maxY - 20;
+          x = maxX - 60;
+        } else if(x >= maxX - 60 && y <= 0){
+          y = 5;
+          x = maxX - 60;
+        } else if(x <= 0){
+          x = 5;
+        } else if(y <= 0){
+          y = 5;
+        } else if(x >= maxX - 60){
+          x = maxX - 60;
+        } else if(y >= maxY - 20){
+          y = maxY - 20;
+        }
 
         element.css({
           top: y + 'px',
