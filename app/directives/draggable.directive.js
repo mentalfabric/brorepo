ae46d71c7d6986e497b0420c796d501f6d272eb2
Michael Wallert <michael.wallert@hotmail.com>
2016-12-13 19:55:57 -0800

    We now have a draggable item!

---
+++ app/directives/draggable.directive.js
@@ -0,0 +1,31 @@
+angular.module('drag', [])
+  .directive('draggable', draggable);
+
+  function draggable($document){
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0, max-y = jQuery('.container--box').css('height'), max-x = jQuery('.container--box').css('width');
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        startX = event.screenX - x;
+        startY = event.screenY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.screenY - startY;
+        x = event.screenX - startX;
+
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+    };
+  }
