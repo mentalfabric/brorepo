fdd5c5f546f533203d5f089862600132b090a294
Michael Wallert <michael.wallert@hotmail.com>
2016-12-15 18:48:06 -0800

    Some stuff

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.get('/', function (req, res) {
-  res.render('index', {welcome: 'Bitwise says hello.'});
+  res.render('index');
 });
 
 app.use('/guests', guests);
